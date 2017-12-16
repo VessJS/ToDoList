9dcd71ce4d8cb267d7570cb28fe3403b6a8f6fa8
Marcin Andrzejewski <andrzejewski.m4@gmail.com>
2017-12-16 15:12:02 +0100

    Adding new features
    Prevent of refreshing on F5, added 'clear all' button

--- ToDo.js
+++ ToDo.js
@@ -1,8 +1,9 @@
 $(function () {
 	$("#add").on("click", function (e) {
-	    e.preventDefault();
+		e.preventDefault();
 		var val = $("input").val();
-		if (val !== '') {
+		if (val !== '') // Prevent from submit when input is empty 
+		{
 			var elem = $("<li></li>").text(val);
 			$(elem).append("<button class='rem'>X</button>");
 			$("#mylist").append(elem);
